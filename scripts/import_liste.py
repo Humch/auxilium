f2f450fa38543b514a2b56a247d34ed4888cdd29
Humch <fabienshlegel@yahoo.fr>
2016-03-28 20:49:50 +0200

    ajout

--- scripts/import_liste.py
+++ scripts/import_liste.py
@@ -24,76 +24,95 @@ from liste_de_course.models import Magasin, Rayon, Marque, Categorie, Article, L
 
 chemin = 'data'
 
-fichiers = [f for f in listdir(chemin) if isfile(join(chemin, f))]
+def creer_rayon(nom):
+    
+    rayon, created = Rayon.objects.get_or_create(nom = nom)
+                
+    if created:
+        
+        print('rayon ajouté :  %s' % rayon)
+    
+    else:
+        
+        print('rayon existe déjà :  %s' % rayon)
+    
+    return created
 
-for fichier in fichiers:
-    
-    print('ouverture de :  %s' % fichier)
-    
-    with open(join(chemin, fichier)) as f:
-        soup = BeautifulSoup(f.read(),'html.parser')
-        
-    for table in soup.find_all('table',attrs={'class':'deviceWidth tabRayon'}):
-        
-        tab_rayon = table.find('font',attrs={'style':'text-transform:uppercase'}).contents
-        
-        print(tab_rayon[0])
-        
-        produits = table.find_all('td',attrs={'width':'61%','style':'font-family:Arial, Helvetica, sans-serif; color:#2a2121; font-size:11px; padding-left:10px','class':'designation'})
-        quantites = table.find_all('td',attrs={'width':'12%','style':'font-family:Arial, Helvetica, sans-serif; color:#2a2121; font-size:11px; text-align:center','class':"quantite"})
-        
-        for produit,quantite in zip(produits,quantites):
-            
-            print('-->' + produit.contents[0] + ' : ' + quantite.contents[0])
+def creer_categorie(nom):
 
-            rayon, created = Rayon.objects.get_or_create(nom = tab_rayon[0])
-            
-            if created:
+    categorie, created = Categorie.objects.get_or_create(nom = nom)
                 
-                print('rayon ajouté :  %s' % rayon)
-            
-            else:
-                
-                print('rayon existe déjà :  %s' % rayon)
-                
-            categorie, created = Categorie.objects.get_or_create(nom = tab_rayon[0])
-            
-            if created:
-                
-                print('categorie ajouté :  %s' % categorie)
-            
-            else:
-                
-                print('categorie existe déjà :  %s' % categorie)
-            
-            marque, created = Marque.objects.get_or_create(nom = produit.contents[0].split(' ', 1)[0])
-            
-            if created:
-                
-                print('marque ajouté :  %s' % marque)
-            
-            else:
-                
-                print('marque existe déjà :  %s' % marque)
+    if created:
+        
+        print('categorie ajouté :  %s' % categorie)
+    
+    else:
+        
+        print('categorie existe déjà :  %s' % categorie)
+    
+    return created
 
-            article, created = Article.objects.get_or_create(nom = produit.contents[0], rayon = rayon, marque = marque)
-            
-            article.categorie.add(categorie)
-            
-            article.save()
-            
-            if created:
-                
-                print('article ajouté :  %s' % produit)
-            
-            else:
-                
-                print('existe déjà :  %s' % produit)
-            
-            
+def creer_marque():
     
-    logging.info('fichier traité :  %s' % fichier)
-            
-#if __name__ == '__main__':
+    marque, created = Marque.objects.get_or_create(nom = nom)
+                
+    if created:
+        
+        print('marque ajouté :  %s' % marque)
     
-#    pass
+    else:
+        
+        print('marque existe déjà :  %s' % marque)
+        
+    return created
+
+def creer_article():
+    
+    article, created = Article.objects.get_or_create(nom = produit.contents[0], rayon = rayon, marque = marque)
+    
+    article.categorie.add(categorie)
+    
+    article.save()
+    
+    if created:
+        
+        print('article ajouté :  %s' % produit)
+    
+    else:
+        
+        print('existe déjà :  %s' % produit)
+        
+
+def import_liste():
+
+    fichiers = [f for f in listdir(chemin) if isfile(join(chemin, f))]
+    
+    for fichier in fichiers:
+        
+        print('ouverture de :  %s' % fichier)
+        
+        with open(join(chemin, fichier)) as f:
+            soup = BeautifulSoup(f.read(),'html.parser')
+            
+        for table in soup.find_all('table',attrs={'class':'deviceWidth tabRayon'}):
+            
+            tab_rayon = table.find('font',attrs={'style':'text-transform:uppercase'}).contents
+            
+            print(tab_rayon[0])
+            
+            produits = table.find_all('td',attrs={'width':'61%','style':'font-family:Arial, Helvetica, sans-serif; color:#2a2121; font-size:11px; padding-left:10px','class':'designation'})
+            quantites = table.find_all('td',attrs={'width':'12%','style':'font-family:Arial, Helvetica, sans-serif; color:#2a2121; font-size:11px; text-align:center','class':"quantite"})
+            
+            for produit,quantite in zip(produits,quantites):
+                
+                print('-->' + produit.contents[0] + ' : ' + quantite.contents[0])
+    
+                creer_rayon(tab_rayon[0])
+                    
+                creer_categorie(tab_rayon[0])
+                
+                creer_marque(produit.contents[0].split(' ', 1)[0])
+    
+if __name__ == '__main__':
+    
+    import_liste()

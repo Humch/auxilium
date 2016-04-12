434a06de23ace1820fb1cba8a622321b605a6f99
Humch <fabienshlegel@yahoo.fr>
2016-04-12 19:05:52 +0200

    modification de la class ListeUpdate pour coller au model Liste

--- liste_de_course/views.py
+++ liste_de_course/views.py
@@ -145,7 +145,7 @@ class ListeCreate(CreateView):
     
 class ListeUpdate(UpdateView):
     model = Liste
-    fields = ['nom','par_defaut','active','archive','magasin']
+    fields = ['nom','par_defaut','active','archive','magasin','propriete_de']
 
     @method_decorator(login_required)
     def dispatch(self, *args, **kwargs):

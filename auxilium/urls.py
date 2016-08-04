582bdf7a0260b0502e041f0d179656dac65ceb18
Fabien Schlegel <fabienschlegel@yahoo.fr>
2016-08-04 11:52:35 +0200

    modification des requirements et correction de login dans urls.py après mise à jour

--- auxilium/urls.py
+++ auxilium/urls.py
@@ -16,7 +16,7 @@ from .views import HomeView, create_article, create_liste
 from liste_de_course import urls
 
 urlpatterns = [
-    url(r'^accounts/login/$', auth_views.login),
+    url(r'^accounts/login/$', auth_views.login, name='login'),
     url(r'^accounts/logout/$', auth_views.logout, name='logout', kwargs={'next_page': '/'}),
     url(r'^$', HomeView.as_view()),
     url(r'^create_article/', create_article, name='create_article'),
